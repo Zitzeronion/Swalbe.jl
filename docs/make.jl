450c9175b79ed69e6271464e17e612e543d3a060
Zitzeronion <physiknerd@gmail.com>
2020-11-17 11:55:11 +0100

    I have no clue why the docs do not deploy

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,18 @@
 using Documenter, Swalbe
 
+doctest(Swalbe; manual = false)
+
 makedocs(;
     modules=[Swalbe],
     authors="Zitzeronion <physiknerd@gmail.com>",
     repo="https://github.com/Ziteronion/Swalbe.jl/blob/{commit}{path}#L{line}",
     sitename="Swalbe",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
+        prettyurls=get(ENV, "CI", nothing) == "true",
         canonical="https://Ziteronion.github.io/Swalbe.jl",
         assets=String[],
     ),
+    checkdocs = :exports,
     pages=[
         "Home" => "index.md",
     ],
