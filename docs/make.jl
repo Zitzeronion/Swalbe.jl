30faf8a69481850d30565a423647bc0a146f9f5b
Zitzeronion <physiknerd@gmail.com>
2020-11-17 14:22:24 +0100

    Maybe I get travis to generate docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 using Documenter, Swalbe
 
-doctest(Swalbe; manual = false)
+# doctest(Swalbe; manual = false)
 
 makedocs(;
     modules=[Swalbe],
@@ -8,9 +8,9 @@ makedocs(;
     repo="https://github.com/Ziteronion/Swalbe.jl/blob/{commit}{path}#L{line}",
     sitename="Swalbe",
     format=Documenter.HTML(;
-        # prettyurls=get(ENV, "CI", nothing) == "true",
-        # canonical="https://Ziteronion.github.io/Swalbe.jl",
-        # assets=String[],
+        prettyurls=get(ENV, "CI", nothing) == "true",
+        canonical="https://Ziteronion.github.io/Swalbe.jl",
+        assets=String[],
     ),
     # checkdocs = :exports,
     pages=[
