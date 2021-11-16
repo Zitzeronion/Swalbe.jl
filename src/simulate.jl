d268fba8dab3e2c5d16e811b0265c1d693c10a29
Stefan Zitz <s.zitz@fz-juelich.de>
2021-11-16 16:46:29 +0100

    Okay now

--- src/simulate.jl
+++ src/simulate.jl
@@ -453,46 +453,6 @@ function run_dropletforced(sys::SysConst_1D; radius=20, θ₀=1/6, center=(sys.L
 
 end
 
-# Fixed, factor 1/2 in front of the slippage
-function run_drop_coal(
-    sys::Swalbe.SysConst_1D;
-    r₁=115,
-    r₂=115, 
-    θ₀=1/9,  
-    verbos=true, 
-    dump = 100, 
-    fluid=zeros(sys.Tmax÷dump, sys.L),
-)
-    println("Simulating droplet coalecense")
-    state = Swalbe.Sys(sys)
-    drop_cent = (sys.L/3, 2*sys.L/3)
-    state.height .= Swalbe.two_droplets(sys, r₁=r₁, r₂=r₂, θ₁=θ₀, θ₂=θ₀, center=drop_cent)
-    Swalbe.equilibrium!(state)
-    println("Starting the lattice Boltzmann time loop")
-    for t in 1:sys.Tmax
-        if t % sys.tdump == 0
-            mass = 0.0
-            mass = sum(state.height)
-            if verbos
-                println("Time step $t bridge height is $(round(state.height[Int(sys.L/2)], digits=3))")
-            end
-        end
-        # Film pressure with rho field
-        Swalbe.filmpressure!(state, sys)
-        Swalbe.h∇p!(state)
-        Swalbe.slippage!(state, sys)
-        # Here we a force that is like pull of an inclined plane
-        state.F .= -state.h∇p .- state.slip # .+ thermal 
-        Swalbe.equilibrium!(state)
-        Swalbe.BGKandStream!(state)
-        Swalbe.moments!(state)
-        # Make a snaphot of the configuration
-        Swalbe.snapshot!(fluid, state.height, t, dumping = dump)
-    end
-    return fluid
-    
-end
-
 # function run_active_thin_film(
 #     sys::SysConst_1D;
 #     h₀=1,
